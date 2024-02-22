3b0e7fd2aa00a276266a4621330a01ef7f73a734
Expcod <118581466+Expcod@users.noreply.github.com>
2024-02-22 12:00:55 +0500

    end

--- api/views.py
+++ api/views.py
@@ -5,10 +5,6 @@ from rest_framework.decorators import api_view
 
 from . import serializers
 
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework import status
-
 @api_view(['POST'])
 def staff_create(request):
     staff = models.Staff.objects.all()
@@ -23,12 +19,12 @@ def list_staff(request):
 
 @api_view(['POST'])
 def attendance_create(request):
-    staff = models.Staff.objects.all()
+    staff = models.Attendance.objects.all()
     serializer = serializers.ListAttendanceSerializer(staff, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def attendance_day(request):
-    staff = models.Staff.objects.all()
-    serializer = serializers.ListStaffSerializer(staff, many=True)
+    staff = models.Attendance.objects.all()
+    serializer = serializers.ListAttendanceSerializer(staff, many=True)
     return Response(serializer.data)
